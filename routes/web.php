4649d0254e513a47b80b217cf2d699125d169fe3
Douglas Soares Cisneiros <douglas@lifecti.com.br>
2021-03-22 18:33:37 -0300

    Commit Inicial.

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\Teste as TesteController;
+use Illuminate\Support\Facades\Route;
+use App\Models\Provider;
+use App\Models\User;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+Route::get('/forgot-password', function () {
+    return view('forgot-password');
+})->name('forgot-password');
+
+Route::get(
+    '/test',
+    [TesteController::class, 'create']
+)->name('profile');
