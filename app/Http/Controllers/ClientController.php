8cb10971f97f6a4162e25c89af60d135ce181c72
Douglas Soares Cisneiros <douglas@lifecti.com.br>
2021-04-10 15:08:29 -0300

    Função para exibir todos os clientes.

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ClientController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = Client::all();
+
+        return view('clients.index', [
+            'users' => $users
+        ]);
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function remove()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
 }
