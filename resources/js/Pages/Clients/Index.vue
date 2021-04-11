dafa302e57fe282303fba66e88cb3575d5cd4faf
Douglas Soares Cisneiros <douglas@lifecti.com.br>
2021-04-10 22:19:06 -0300

    Exibição dos clientes cadastrados.

---
+++ resources/js/Pages/Clients/Index.vue
@@ -0,0 +1,55 @@
+<template>
+  <app-layout>
+    <template #header>
+      <div class="flex justify-between">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          Clientes cadastrados
+        </h2>
+
+        <inertia-link
+          class="inline-flex items-center px-4 py-2 bg-green-400 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150"
+          :href="route('clients.create')"
+          as="button"
+          type="button"
+        >
+          Cadastrar Cliente
+        </inertia-link>
+      </div>
+    </template>
+    <div>
+      <table class="table-auto">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Telefone</th>
+            <th>Endereço</th>
+          </tr>
+        </thead>
+        <tbody v-for="client in clients" :key="client.id">
+          <tr>
+            <td>{{ client.id }}</td>
+            <td>{{ client.name }}</td>
+            <td>{{ client.email }}</td>
+            <td>{{ client.phone }}</td>
+            <td>{{ client.address }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </app-layout>
+</template>
+<script>
+import AppLayout from "@/Layouts/AppLayout";
+import JetButton from "@/Jetstream/Button";
+export default {
+  components: {
+    AppLayout,
+    JetButton,
+  },
+  props: {
+    clients: Array,
+  },
+};
+</script>
