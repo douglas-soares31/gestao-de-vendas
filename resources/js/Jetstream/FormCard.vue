c2058172d4768a381d1d8d1f5278c39835c56480
Douglas Soares Cisneiros <douglas@lifecti.com.br>
2021-04-10 22:19:53 -0300

    Novos componentes.

---
+++ resources/js/Jetstream/FormCard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="w-full object-center sm:max-w-lg mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg"
+  >
+    <slot name="form"></slot>
+  </div>
+</template>
---
+++ resources/js/Jetstream/Textarea.vue
@@ -0,0 +1,8 @@
+<template>
+  <textarea
+    id="address"
+    type="textarea"
+    class="textarea border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+  >
+  </textarea>
+</template>
