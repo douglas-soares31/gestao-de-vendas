0a4d76cc73efa643104d1e2b4336c00d245ad7e8
Douglas Soares Cisneiros <douglas@lifecti.com.br>
2021-04-14 00:09:46 -0300

    Ajuste no tamanho do formulário.

--- resources/js/Jetstream/FormCard.vue
+++ resources/js/Jetstream/FormCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full object-center sm:max-w-lg mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg"
+    class="w-full object-center lg:max-w-lg mt-6 px-6 py-4 shadow-md overflow-hidden lg:rounded-lg"
   >
     <slot name="form"></slot>
   </div>
