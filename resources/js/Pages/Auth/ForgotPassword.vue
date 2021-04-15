e258662b0c8c48bf26132c308951f779363ef250
Douglas Soares Cisneiros <douglas@lifecti.com.br>
2021-04-15 00:27:21 -0300

    Ajuste na formatação.

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -1,68 +1,79 @@
 <template>
-    <jet-authentication-card>
-        <template #logo>
-            <jet-authentication-card-logo />
-        </template>
+  <jet-authentication-card>
+    <template #logo>
+      <jet-authentication-card-logo />
+    </template>
 
-        <div class="mb-4 text-sm text-gray-600">
-            Esqueceu sua senha? Sem problema, nos informe o seu email que utiliza para acessar, que mandaremos um link para a redefinição da senha.
-        </div>
+    <div class="mb-4 text-sm text-gray-600">
+      Esqueceu sua senha? Sem problema, nos informe o seu email que utiliza para
+      acessar, que mandaremos um link para a redefinição da senha.
+    </div>
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
+    <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+      {{ status }}
+    </div>
 
-        <jet-validation-errors class="mb-4" />
+    <jet-validation-errors class="mb-4" />
 
-        <form @submit.prevent="submit">
-            <div>
-                <jet-label for="email" value="Email" />
-                <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus />
-            </div>
+    <form @submit.prevent="submit">
+      <div>
+        <jet-label for="email" value="Email" />
+        <jet-input
+          id="email"
+          type="email"
+          class="mt-1 block w-full"
+          v-model="form.email"
+          required
+          autofocus
+        />
+      </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Confirmar
-                </jet-button>
-            </div>
-        </form>
-    </jet-authentication-card>
+      <div class="flex items-center justify-end mt-4">
+        <jet-button
+          :class="{ 'opacity-25': form.processing }"
+          :disabled="form.processing"
+        >
+          Confirmar
+        </jet-button>
+      </div>
+    </form>
+  </jet-authentication-card>
 </template>
 
 <script>
-    import JetAuthenticationCard from '@/Jetstream/AuthenticationCard'
-    import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo'
-    import JetButton from '@/Jetstream/Button'
-    import JetInput from '@/Jetstream/Input'
-    import JetLabel from '@/Jetstream/Label'
-    import JetValidationErrors from '@/Jetstream/ValidationErrors'
+import JetAuthenticationCard from "@/Jetstream/AuthenticationCard";
+import JetAuthenticationCardLogo from "@/Jetstream/AuthenticationCardLogo";
+import JetButton from "@/Jetstream/Button";
+import JetInput from "@/Jetstream/Input";
+import JetLabel from "@/Jetstream/Label";
+import JetValidationErrors from "@/Jetstream/ValidationErrors";
 
-    export default {
-        components: {
-            JetAuthenticationCard,
-            JetAuthenticationCardLogo,
-            JetButton,
-            JetInput,
-            JetLabel,
-            JetValidationErrors
-        },
+export default {
+  components: {
+    JetAuthenticationCard,
+    JetAuthenticationCardLogo,
+    JetButton,
+    JetInput,
+    JetLabel,
+    JetValidationErrors,
+  },
 
-        props: {
-            status: String
-        },
+  props: {
+    status: String,
+  },
 
-        data() {
-            return {
-                form: this.$inertia.form({
-                    email: ''
-                })
-            }
-        },
+  data() {
+    return {
+      form: this.$inertia.form({
+        email: "",
+      }),
+    };
+  },
 
-        methods: {
-            submit() {
-                this.form.post(this.route('password.email'))
-            }
-        }
-    }
+  methods: {
+    submit() {
+      this.form.post(this.route("password.email"));
+    },
+  },
+};
 </script>
