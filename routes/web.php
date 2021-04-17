9a14fa131ba133df9949f61e599173c67cd93dd4
Douglas Soares Cisneiros <douglas@lifecti.com.br>
2021-04-17 09:27:20 -0300

    Inclusão das rotas de cadastros.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ProviderController;
+use App\Http\Controllers\RegisterExpenseController;
+use App\Http\Controllers\RegisterProductController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -31,11 +34,42 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
 
-    Route::prefix('clients')->group(function () {
-        Route::get('/', [ClientController::class, 'index'])->name('clients');
-        Route::get('/create', [ClientController::class, 'create'])->name('clients.create');
-        Route::post('/create', [ClientController::class, 'store']);
-        Route::post('/remove/{id}', [ClientController::class, 'remove'])->name('clients.remove');
-        Route::get('/update', [ClientController::class, 'update'])->name('clients.update');
+    Route::prefix('register')->group(function () {
+
+        Route::prefix('clients')->group(function () {
+            Route::get('/', [ClientController::class, 'index'])->name('clients');
+            Route::get('/create', [ClientController::class, 'create'])->name('clients.create');
+            Route::post('/', [ClientController::class, 'store'])->name('clients.store');
+            Route::delete('/{id}/remove', [ClientController::class, 'remove'])->name('clients.remove');
+            Route::get('/{id}/edit', [ClientController::class, 'edit'])->name('clients.edit');
+            Route::put('/{id}/update', [ClientController::class, 'update'])->name('clients.update');
+        });
+
+        Route::prefix('providers')->group(function () {
+            Route::get('/', [ProviderController::class, 'index'])->name('providers');
+            Route::get('/create', [ProviderController::class, 'create'])->name('providers.create');
+            Route::post('/', [ProviderController::class, 'store'])->name('providers.store');
+            Route::delete('/{id}/remove', [ProviderController::class, 'remove'])->name('providers.remove');
+            Route::get('/{id}/edit', [ProviderController::class, 'edit'])->name('providers.edit');
+            Route::put('/{id}/update', [ProviderController::class, 'update'])->name('providers.update');
+        });
+
+        Route::prefix('expenses')->group(function () {
+            Route::get('/', [RegisterExpenseController::class, 'index'])->name('register.expenses');
+            Route::get('/create', [RegisterExpenseController::class, 'create'])->name('register.expenses.create');
+            Route::post('/', [RegisterExpenseController::class, 'store'])->name('register.expenses.store');
+            Route::delete('/{id}/remove', [RegisterExpenseController::class, 'remove'])->name('register.expenses.remove');
+            Route::get('/{id}/edit', [RegisterExpenseController::class, 'edit'])->name('register.expenses.edit');
+            Route::put('/{id}/update', [RegisterExpenseController::class, 'update'])->name('register.expenses.update');
+        });
+
+        Route::prefix('products')->group(function () {
+            Route::get('/', [RegisterProductController::class, 'index'])->name('register.products');
+            Route::get('/create', [RegisterProductController::class, 'create'])->name('register.products.create');
+            Route::post('/', [RegisterProductController::class, 'store'])->name('register.products.store');
+            Route::delete('/{id}/remove', [RegisterProductController::class, 'remove'])->name('register.products.remove');
+            Route::get('/{id}/edit', [RegisterProductController::class, 'edit'])->name('register.products.edit');
+            Route::put('/{id}/update', [RegisterProductController::class, 'update'])->name('register.products.update');
+        });
     });
 });
