95299808cca30f4d5b2a801ff5e06bcd3b56640f
Douglas Soares Cisneiros <douglas@lifecti.com.br>
2021-04-17 09:25:17 -0300

    Crud dos Fornecedores.

--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Provider;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ProviderController extends Controller
 {
@@ -14,7 +15,11 @@ class ProviderController extends Controller
      */
     public function index()
     {
-        //
+        $providers = Provider::all();
+
+        return Inertia::render('Register/Providers/Index', [
+            'providers' => $providers
+        ]);
     }
 
     /**
@@ -24,7 +29,7 @@ class ProviderController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Register/Providers/Create');
     }
 
     /**
@@ -35,7 +40,15 @@ class ProviderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $provider = new Provider;
+
+        $provider->name = $request->name;
+        $provider->phone = $request->phone;
+        $provider->address = $request->address;
+
+        $provider->save();
+
+        return redirect()->action([ProviderController::class, 'index']);
     }
 
     /**
@@ -55,9 +68,13 @@ class ProviderController extends Controller
      * @param  \App\Models\Provider  $provider
      * @return \Illuminate\Http\Response
      */
-    public function edit(Provider $provider)
+    public function edit($id)
     {
-        //
+        $provider = Provider::find($id);
+
+        return Inertia::render('Register/Providers/Edit', [
+            'provider' => $provider
+        ]);
     }
 
     /**
@@ -69,7 +86,15 @@ class ProviderController extends Controller
      */
     public function update(Request $request, Provider $provider)
     {
-        //
+        $provider = Provider::find($request->id);
+
+        $provider->name = $request->name;
+        $provider->phone = $request->phone;
+        $provider->address = $request->address;
+
+        $provider->save();
+
+        return redirect()->action([ProviderController::class, 'index']);
         
     }
 
@@ -79,8 +104,12 @@ class ProviderController extends Controller
      * @param  \App\Models\Provider  $provider
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Provider $provider)
+    public function remove($id)
     {
-        //
+        $providers = Provider::find($id);
+
+        $providers->delete();
+
+        return redirect()->action([ProviderController::class, 'index']);
     }
 }
